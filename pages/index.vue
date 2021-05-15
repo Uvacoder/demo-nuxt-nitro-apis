cd7e98d82036f0960b4078574e887d4df2537117
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2021-05-15 20:18:17 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+     <h2>/api/post</h2>
+    <pre>{{ post }}</pre>
+
+    <br>
+
+    <h2>/api/posts</h2>
+    <pre>{{ posts }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: null,
+      posts: [],
+    }
+  },
+  async created() {
+    this.post = await $fetch('/api/post')
+    this.posts = await $fetch('/api/posts')
+  }
+}
+</script>
